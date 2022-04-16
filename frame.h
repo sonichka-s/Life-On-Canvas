a27b916c6a24003e6d18648e8f46e98b07cf5ec7
Vladimir <58148455+Gallaann@users.noreply.github.com>
2022-04-16 03:08:01 +0300

    Add code framework

---
+++ frame.h
@@ -0,0 +1,35 @@
+//
+// Created by gallaann on 16.04.22.
+//
+
+#ifndef UNTITLED_FRAME_H
+#define UNTITLED_FRAME_H
+
+struct Color{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+};
+
+struct Position{
+    unsigned int x;
+    unsigned int y;
+};
+
+class DataBase{
+private:
+    struct Pixel{
+        Color color;
+        Position position;
+    };
+
+    void Connect(); // connect to DB
+    void Create(); // create canvas
+    void Read(); // not sure for this
+    void Insert(Pixel pixel); // pull changes to canvas/DB
+    void Update(Pixel pixel); // push changes to other users
+    void Delete(); // for delete canvas
+    void CloseConnection(); // break the connection
+};
+
+#endif //UNTITLED_FRAME_H
