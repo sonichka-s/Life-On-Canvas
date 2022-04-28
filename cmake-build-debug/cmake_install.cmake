a6323d54f9bb2797080459c9d3c127e5f244b34f
sonichka-s <stnchnk0@gmail.com>
2022-04-28 10:27:35 +0300

    work on api

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/sonichka/Life-On-Canvas
+# Install script for directory: /home/sonichka/Документы/Life-On-Canvas
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -44,7 +44,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/sonichka/Life-On-Canvas/cmake-build-debug/src/cmake_install.cmake")
+  include("/home/sonichka/Документы/Life-On-Canvas/cmake-build-debug/src/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -55,5 +55,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/sonichka/Life-On-Canvas/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/sonichka/Документы/Life-On-Canvas/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
