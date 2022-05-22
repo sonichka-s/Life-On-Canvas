fa6e7e91be0b13451c68e231ad4251de1c465562
Ibragim Dzaurov <100013546+IbragimDzaurov@users.noreply.github.com>
2022-05-22 22:14:24 +0300

    Add files via upload

---
+++ LifeOnCanvas/mainscene.h
@@ -0,0 +1,47 @@
+#ifndef MAINSCENE_H
+#define MAINSCENE_H
+#include <QGraphicsScene>
+#include <QGraphicsItem>
+#include <QMouseEvent>
+
+
+class MainScene : public QGraphicsScene
+{
+public:
+    MainScene();
+
+    void setType(QString t);    // Установка типа чертежа
+    QString getType();          // Получить тип чертежа
+    void setRect(QRectF rect);  // Установка области обновления
+    QRectF getRect();           // Получить область обновления
+    void setPen(QPen *p);       // Набор кистей
+    QPen *getPen();             // Получить кисть
+    void setBrush(QBrush *b);   // Установить кисть
+    QBrush *getBrush();         // Получить кисть
+    void setFont(QFont f);      // Установить шрифт
+    QFont getFont();            // Получить шрифт
+    void setText(QString t);
+    void deleteOne();           // Возврат к предыдущей операции
+    void deleteAll();           // Очистить  доску
+    void mousePressd(QMouseEvent *event);   // Реагировать на событие нажатия мыши
+    void mouseMoved(QMouseEvent *event);    // Реагировать на события перемещения мыши
+    void mouseReleased(QMouseEvent *event); // Реагировать на событие отпускания мыши
+
+private:
+    QString draw_type = "None";    // Тип чертежа метки
+    QRectF draw_rect;              // Область обновления просмотра хранилища
+    QPen *pen;                     // Основная кисть
+    QPen *pen_eraser;               // Кисть, используемая для реализации ластика
+    QBrush *brush;                  // Основная кисть
+    QBrush *brush_text;
+    QFont font;                    
+    QString text;                   // Текстовое содержимое чертежа
+    QPoint start_point, end_point;  // Начальная и конечная точки щелчков мыши
+    QVector<QGraphicsItem*> items;  // Сохранить все элементы, добавленные во время рисования
+    QVector<QPoint> polygonPoints;  // Сохранить все точки, полученные щелчком мыши во время рисования полигона
+    QVector<QGraphicsLineItem*> polygonLines;   // Временное сохранение сегментов линий, созданных во время рисования полигонов
+    QPoint freePointLast;          // Сохранение предыдущей конечной точки при рисовании свободной кривой
+    QGraphicsItemGroup* freeGroup;  // Коллекция элементов, используемая для хранения всех сегментов линий, созданных при рисовании свободных кривых
+};
+
+#endif // MAINSCENE_H
