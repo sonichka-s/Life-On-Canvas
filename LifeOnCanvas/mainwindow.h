2454696299b414487246a5cc67c858322413aa74
Ibragim Dzaurov <100013546+IbragimDzaurov@users.noreply.github.com>
2022-05-22 22:12:48 +0300

    Add files via upload

---
+++ LifeOnCanvas/mainwindow.h
@@ -0,0 +1,104 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QInputDialog>
+#include <QFontDialog>
+#include <QColorDialog>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QLabel>
+#include <QScreen>
+#include "mainscene.h"
+#include "mainview.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    // Коллекция имен стилей кисти, отсортированных в соответствии с Qt::PenStyle
+    const QString pen_styles[6]= {"NoPen","SolidLine","DashLine",
+                                  "DotLine","DashDotLine","DashDotDotLine"};
+    // Коллекция имен стилей кисти, отсортированных в соответствии с Qt::BrushStyle
+    const QString brush_styles[15] = {"NoBrush", "SolidPattern", "Dense1Pattern",
+                                      "Dense2Pattern", "Dense3Pattern", "Dense4Pattern",
+                                      "Dense5Pattern", "Dense6Pattern", "Dense7Pattern",
+                                      "HorPattern", "VerPattern", "CrossPattern",
+                                     "BDiagPattern", "FDiagPattern", "DiagCrossPattern"};
+
+protected:
+    void resizeEvent(QResizeEvent *event);
+
+private slots:
+    void on_actionAbout_Me_triggered();     // Отображение информации “Обо мне”
+
+    void on_actionAbout_QT_triggered();     // Отображение информации о Qt
+
+    void on_actionLine_triggered();         // Установить для рисования прямой линии
+
+    void on_actionRectangle_triggered();    // Установить для рисования прямоугольника
+
+    void on_actionEllipse_triggered();      // Установить для рисования эллипса
+
+    void on_actionPolygon_triggered();      // Установить для рисования полигонов
+
+    void on_actionFree_triggered();         // Установить для рисования свободной кривой
+
+
+    void on_actionEraser_triggered();       // Установить в режим ластика
+
+    void on_actionCancel_triggered();       // Возврат к предыдущей операции
+
+
+    void on_actionFont_triggered();         // Установка шрифта для рисования текста
+
+    void on_actionLineWidth_triggered();    // Установите ширину линии кисти
+
+    void on_actionLineColor_triggered();    // Установить цвет кисти
+
+    void on_actionNoPen_triggered();        // Установить тип линии кисти на беспроводную полосу
+
+    void on_actionSolidLine_triggered();    // Установить тип линии кисти на сплошную линию
+
+    void on_actionDashLine_triggered();     // Установить тип линии кисти на пунктирную линию
+
+    void on_actionDotLine_triggered();      // Установить тип линии кисти на точечную линию DotLine
+
+    void on_actionDashDotLine_triggered();  // Установить тип линии кисти на пунктирную точечную линию DashDotLine
+
+    void on_actionDashDotDotLine_triggered();// Пунктирная линия  DashDotDotLine
+
+
+
+    void on_actionSave_As_triggered();     // Сохранить доску в виде рисунка
+
+private:
+    void init_action_groups();              // Инициализировать группировку кнопок действий
+    void init_status_bar();               // Инициализировать нижнюю строку состояния
+    void update_status_text();             // Обновить текстовую информацию в строке состояния
+
+private:
+    Ui::MainWindow *ui;
+
+    // Использовать Graphics View Framework как чертежная архитектура
+    MainScene *scene;   // QGraphicsScene Часть (сцена)
+    MainView *view;     // QGraphicsView Часть (вид)
+    QPen *pen;          // Основная кисть
+    QBrush *brush;      // Основная кисть (кисть используется для заполнения внутренней области рисунка)
+
+    // Сохранить соответствующую текстовую информацию в строке состояния в нижней части окна
+    QLabel *status_text_pen_color;
+    QLabel *status_text_pen_style;
+    QLabel *status_text_pen_width;
+    QLabel *status_text_brush_color;
+    QLabel *status_text_brush_style;
+};
+#endif // MAINWINDOW_H
